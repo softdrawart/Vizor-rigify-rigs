5ea9ceb44673c3f5b51409faf4c8d5b651934302
Mikhail Lebedev <softdrawart@gmail.com>
2025-03-17 18:50:47 +0200

    global bone to switch parent to

---
+++ rigs/basic/global.py
@@ -0,0 +1,85 @@
+import bpy
+from bpy.types import PoseBone
+
+from rigify.base_rig import BaseRig, stage
+from rigify.utils.naming import make_derived_name
+from rigify.utils.widgets import layout_widget_dropdown, create_registered_widget
+from rigify.utils.switch_parent import SwitchParentBuilder
+
+class Rig(BaseRig):
+    '''Controll bone with ability to Parent Switch between controllers from all rigs.
+       Usefull for Props that need to be dynamically re-Parented.'''
+    
+    bones: BaseRig.ToplevelBones[
+        str,
+        'Rig.CtrlBones',
+        'Rig.MchBones',
+        str
+    ]
+    make_controller: bool
+    make_deformer: bool
+
+    def find_org_bones(self, bone: PoseBone):
+        return bone.name
+    
+    def initialize(self):
+        self.make_controller = self.params.make_controller
+        self.make_deformer = self.params.make_deformer
+
+
+    #generate CTRL bones and Switch Parent
+    def generate_bones(self):
+        #create controller bone
+        if self.make_controller:
+            org = self.bones.org
+            ctrl = self.bones.ctrl = self.copy_bone(org, make_derived_name(org, 'ctrl'))
+            pbuild = SwitchParentBuilder(self.generator)
+            pbuild.register_parent(self, bone=org, name=ctrl, is_global=True, exclude_self=True)
+        #create deformer bone
+        if self.make_deformer:
+            org = self.bones.org
+            self.bones.deform = self.copy_bone(org, make_derived_name(org, 'def'))
+
+
+    #parent ORG bone to CTRL
+    def parent_bones(self):
+        if self.make_controller:
+            org = self.bones.org
+            ctrl = self.bones.ctrl
+            self.set_bone_parent(org, ctrl)
+    #copy parameters from ORG bones if present
+    def configure_bones(self):
+        if self.make_controller:
+            ctrl = self.bones.ctrl
+            org = self.bones.org
+            self.copy_bone_properties(org, ctrl)
+    #add Copy Tranform constraint of ORG bone to DEF bone
+    def rig_bones(self):
+        if self.make_deformer:
+            org = self.bones.org
+            deform = self.bones.deform
+            self.make_constraint(deform,'COPY_TRANSFORMS', org)
+    #generate widget based on the selection from parameter
+    def generate_widgets(self):
+        if self.make_controller:
+            ctrl = self.bones.ctrl
+            widget = self.params.widget_selection
+            create_registered_widget(self.obj, ctrl, widget)
+    
+
+
+    @classmethod
+    def add_parameters(cls, params):
+        params.make_controller = bpy.props.BoolProperty("Controller", default=False, description="Create Controller Bone")
+        params.make_deformer = bpy.props.BoolProperty("Deformer", default=False, description="Create Deformer Bone")
+        params.widget_selection = bpy.props.StringProperty("Widget", description="Widget of controller bone")
+
+    @classmethod
+    def parameters_ui(cls, layout, params):
+        row = layout.row()
+        row.prop(params,'make_controller')
+
+        if params.make_controller:
+            layout_widget_dropdown(layout, params, 'widget_selection')
+        row = layout.row()
+        row.prop(params,'make_deformer')
